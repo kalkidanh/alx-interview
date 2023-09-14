7934150fa6697b56c7a1fc06393a2c8338122657
kalkidanh <kalkidanh89@gmail.com>
2023-09-14 20:20:00 +0300

    using a different method

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,12 +5,12 @@
 def rotate_2d_matrix(matrix):
     """ Rotate the matrix 90 degrees CW."""
     length = len(matrix)
-
-    """ Transposes the 2D  matrix."""
-    for i in range(length):
-        for j in range(i, length):
-            matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
-
-        """ Reverses each row."""
-        for i in range(length):
-            matrix[i] = matrix[i][::-1]
+    for item in range(length // 2):
+        fst, lst, tpose = item, length - 1 - item, 0
+        for i in range(fst, lst):
+            start = matrix[fst][i]
+            matrix[fst][i] = matrix[lst - tpose][fst]
+            matrix[lst - tpose][fst] = matrix[lst][lst - tpose]
+            matrix[lst][lst - tpose] = matrix[i][lst]
+            matrix[i][lst] = start
+            tpose += 1
