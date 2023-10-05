29dfae194e781ac702331ebbab491a149a93235f
kalkidanh <kalkidanh89@gmail.com>
2023-10-05 20:27:58 +0300

    winner returned

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" determine who the winner of the prime game is."""
+
+
+def findMultiples(num, targets):
+    """ Function to Find the multiples of a given number within a list."""
+    for i in targets:
+        if i % num == 0:
+            targets.remove(i)
+    return targets
+
+
+def isPrime(i):
+    """ Function to check if a number is a prime."""
+    if i == 1:
+        return False
+    for j in range(2, i):
+        if i % j == 0:
+            return False
+    return True
+
+
+def findPrimes(n):
+    """ Function to sort a given set to prime numbers and non-prime numbers."""
+    counter = 0
+    target = list(n)
+    for i in range(1, len(target) + 1):
+        if isPrime(i):
+            counter += 1
+            target.remove(i)
+            target = findMultiples(i, target)
+        else:
+            pass
+    return counter
+
+
+def isWinner(x, nums):
+    """ Function to determine who the winner of each game is."""
+    players = {'Maria': 0, 'Ben': 0}
+    cluster = set()
+    for elem in range(x):
+        nums.sort()
+        num = nums[elem]
+        for i in range(1, num + 1):
+            cluster.add(i)
+            if i == num + 1:
+                break
+        temp = findPrimes(cluster)
+
+        if temp % 2 == 0:
+            players['Ben'] += 1
+        elif temp % 2 != 0:
+            players['Maria'] += 1
+
+    if players['Maria'] > players['Ben']:
+        return 'Maria'
+    elif players['Maria'] < players['Ben']:
+        return 'Ben'
+    else:
+        return None
